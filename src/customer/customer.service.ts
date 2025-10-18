1f7da3c5659cdb040400a8d45d6e6bdb4addc3b9
Md. Humayun Kabir Sobuj <humayun506034@gmail.com>
2025-10-18 17:11:47 +0600

    modify

--- src/customer/customer.service.ts
+++ src/customer/customer.service.ts
@@ -1,6 +1,22 @@
 import { Injectable } from '@nestjs/common';
+import { Customer } from './interfaces/customer.interface';
+import { CreateCustomerDto } from './dto/create-customer.dto';
 
 @Injectable()
 export class CustomerService {
-    
+  private customers: Customer[] = [];
+
+  getAllCustomers(): Customer[] {
+    return this.customers;
+  }
+
+  addCustomer(createCustomerDto: CreateCustomerDto): Customer {
+    const newCustomer = this.customers.push({
+      id: this.customers.length + 1,
+      ...createCustomerDto,
+    }) as any;
+
+    this.customers.push(newCustomer);
+    return newCustomer;
+  }
 }
